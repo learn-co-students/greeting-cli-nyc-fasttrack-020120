743ee7fec6280cc433633a94ebbe58cdb8beb98e
Brandon Lee <eternalartistxii@yahoo.com>
2020-02-04 05:34:27 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,9 @@
 # code the #greeting method here!
 
+
 def greeting(name)
-  puts "Hello #{name}. It's nice to meet you."
+  if name != ""
+    puts "Hi! I'm HAL, what's your name?"
+  else
+    puts "Hello #{name}. It's nice to meet you."
 end
